6049de11c45eee501a18fa4f7f1e4a50662f0f1b
Nathan Lopez <nathancarllopez@gmail.com>
2025-09-02 11:01:38 -0500

    Split UpperHalfPlane point into three distinct types of points, and added classes for geodesics, circles and horocycles

--- src/types-validators/types.ts
+++ src/types-validators/types.ts
@@ -5,9 +5,35 @@ export type ComplexNumber = {
 
 export type PositiveNumber = number & { __brand: "PositiveNumber" };
 
-export interface UpperHalfPlanePoint extends ComplexNumber {
-  re: number;
+export type PointAtInfinity = ComplexNumber & { __brand: "PointAtInfinity" };
+
+export interface UhpBoundaryPoint extends ComplexNumber {
+  im: 0;
+}
+export interface UhpInteriorPoint extends ComplexNumber {
   im: PositiveNumber;
+}
+export type UhpPoint = PointAtInfinity | UhpBoundaryPoint | UhpInteriorPoint;
+
+export type UhpGeodesic = {
+  isVertical: boolean;
+  center: PointAtInfinity | UhpBoundaryPoint; // { re: Infinity, im: Infinity } if isVertical, otherwise a point on the real axis
+  radius: number; // Distance between center and any point on the geodesic, Infinity if isVertical
+  points: UhpPoint[]; // Contains the endpoints of the geodesic and two interior points, ordered by how the points were given (see upper-half-plane/geometry.ts)
+};
+
+export type UhpCircle = {
+  center: UhpInteriorPoint;
+  radius: number;
+  eucCenter: UhpInteriorPoint;
+  eucRadius: number;
+};
+
+export type UhpHorocycle = {
+  center: UhpInteriorPoint | PointAtInfinity;
+  basePoint: UhpBoundaryPoint | PointAtInfinity;
+  bdryPoint: UhpInteriorPoint,
+  eucRadius: number;
 }
 
 export type MobiusTransformation = {
@@ -18,10 +44,3 @@ export type MobiusTransformation = {
 };
 
 export type Isometry = MobiusTransformation & { __brand: "Isometry" };
-
-export type UhpGeodesic = {
-  isVertical: boolean;
-  center: ComplexNumber;    // { re: Infinity, im: Infinity } if isVertical, otherwise a point on the real axis
-  radius: number;           // Distance between center and any point on the geodesic, Infinity if isVertical
-  points: ComplexNumber[];  // Given two UpperHalfPlanePoints z, w, points is the array [bdryPointZ, z, w, bdryPointW]
-};
