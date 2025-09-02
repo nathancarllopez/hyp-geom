50d7cf5a7b416090669f171f3221f3dbad6a54f4
Nathan Lopez <nathancarllopez@gmail.com>
2025-09-02 11:01:54 -0500

    Added validators for the new types of Upper half plane points

--- src/types-validators/validators.ts
+++ src/types-validators/validators.ts
@@ -1,7 +1,24 @@
 import { determinant } from "../general-math/mobius-transformations";
-import { Isometry, MobiusTransformation, PositiveNumber } from "./types";
+import {
+  ComplexNumber,
+  Isometry,
+  MobiusTransformation,
+  PointAtInfinity,
+  PositiveNumber,
+  UhpBoundaryPoint,
+  UhpInteriorPoint,
+} from "./types";
 
 export const isPositiveNumber = (num: number): num is PositiveNumber => num > 0;
+
+export const isPointAtInfinity = (z: ComplexNumber): z is PointAtInfinity =>
+  z.re === Infinity || z.im === Infinity;
+
+export const isUhpBoundaryPoint = (z: ComplexNumber): z is UhpBoundaryPoint =>
+  Number.isFinite(z.re) && z.im === 0;
+
+export const isUhpInteriorPoint = (z: ComplexNumber): z is UhpInteriorPoint =>
+  Number.isFinite(z.re) && Number.isFinite(z.im) && z.im > 0;
 
 export const isIsometry = (m: MobiusTransformation): m is Isometry => {
   const det = determinant(m);
