50c82e1eac9454b329097f9516c4e226dd79e841
Nathan Lopez <nathancarllopez@gmail.com>
2025-09-06 12:47:54 -0500

    1) Changed the validadtors for the UhpPoints to reflect the changes made to they types.ts file (see previous commit) 2) Removed the isIsometry validator as validation is handled by the constructor of UhpIsometry

--- src/types-validators/validators.ts
+++ src/types-validators/validators.ts
@@ -1,12 +1,11 @@
-import { determinant } from "../general-math/mobius-transformations";
 import {
   ComplexNumber,
-  Isometry,
-  MobiusTransformation,
   PointAtInfinity,
   PositiveNumber,
   UhpBoundaryPoint,
   UhpInteriorPoint,
+  UhpPoint,
+  UhpRealLine,
 } from "./types";
 
 export const isPositiveNumber = (num: number): num is PositiveNumber => num > 0;
@@ -14,20 +13,19 @@ export const isPositiveNumber = (num: number): num is PositiveNumber => num > 0;
 export const isPointAtInfinity = (z: ComplexNumber): z is PointAtInfinity =>
   z.re === Infinity || z.im === Infinity;
 
-export const isUhpBoundaryPoint = (z: ComplexNumber): z is UhpBoundaryPoint =>
+export const isOnRealLine = (z: ComplexNumber): z is UhpRealLine =>
   Number.isFinite(z.re) && z.im === 0;
 
 export const isUhpInteriorPoint = (z: ComplexNumber): z is UhpInteriorPoint =>
   Number.isFinite(z.re) && Number.isFinite(z.im) && z.im > 0;
 
-export const isIsometry = (m: MobiusTransformation): m is Isometry => {
-  const det = determinant(m);
-  const isNonZero = det.re !== 0 || det.im !== 0;
+export const isUhpBoundaryPoint = (z: ComplexNumber): z is UhpBoundaryPoint => isPointAtInfinity(z) || isOnRealLine(z);
 
-  if (!isNonZero) {
-    console.log("transformation:", m);
-    console.log("determinant:", det);
-  }
+export const isUhpPoint = (z: ComplexNumber): z is UhpPoint =>
+  isUhpBoundaryPoint(z) || isUhpInteriorPoint(z);
 
-  return isNonZero;
-};
+export const isPointArray = (arg: unknown): arg is [number, number] =>
+  Array.isArray(arg) &&
+  arg.length === 2 &&
+  typeof arg[0] === "number" &&
+  typeof arg[1] === "number";
