010b8697690dad8fbff64a24996cbd12aa0bcf99
Nathan Lopez <nathancarllopez@gmail.com>
2025-09-06 12:46:32 -0500

    1) Changes to UhpPoint types: what was 'UhpBoundaryPoint' is now 'UhpRealLine', while 'UhpBoundaryPoint' is now defined as the union of PointAtInfinity and UhpRealLine. This makes UhpPoint into the union of UhpInteriorPoint and UhpBoundaryPoint. I prefer this since lots of the time I want to refer to the real line and the point at infinity at the same time. 2) Made the points array for UhpGeodesic more explicit by making it into its own type. 3) Added types for geodesic segments, rays, and polygons 4) The types UhpIsometryInfo and UhpConjugateInfo, respectively, are used to describe a general isometry and the conjugacy relationship between two isometries, respectively. They're only used by the UhpIsometry class, though, so I may just move them to that file. Especially because that would mean I don't have to import from that file, which just seems weird to me

--- src/types-validators/types.ts
+++ src/types-validators/types.ts
@@ -1,3 +1,5 @@
+import { UhpIsometry } from "../upper-half-plane/isometries";
+
 export type ComplexNumber = {
   re: number;
   im: number;
@@ -7,20 +9,43 @@ export type PositiveNumber = number & { __brand: "PositiveNumber" };
 
 export type PointAtInfinity = ComplexNumber & { __brand: "PointAtInfinity" };
 
-export interface UhpBoundaryPoint extends ComplexNumber {
+export interface UhpRealLine extends ComplexNumber {
   im: 0;
 }
+
 export interface UhpInteriorPoint extends ComplexNumber {
   im: PositiveNumber;
 }
-export type UhpPoint = PointAtInfinity | UhpBoundaryPoint | UhpInteriorPoint;
 
+export type UhpBoundaryPoint = UhpRealLine | PointAtInfinity
+
+export type UhpPoint = UhpBoundaryPoint | UhpInteriorPoint;
+
+export type UhpGeodesicPoints = [
+  UhpBoundaryPoint | PointAtInfinity,
+  UhpInteriorPoint,
+  UhpInteriorPoint,
+  UhpBoundaryPoint | PointAtInfinity
+]; // Contains the endpoints of the geodesic and two interior points, ordered by how the points were given (see upper-half-plane/geometry.ts)
 export type UhpGeodesic = {
   isVertical: boolean;
   center: PointAtInfinity | UhpBoundaryPoint; // { re: Infinity, im: Infinity } if isVertical, otherwise a point on the real axis
   radius: number; // Distance between center and any point on the geodesic, Infinity if isVertical
-  points: UhpPoint[]; // Contains the endpoints of the geodesic and two interior points, ordered by how the points were given (see upper-half-plane/geometry.ts)
+  points: UhpGeodesicPoints;
 };
+
+export type UhpGeodesicSegment = UhpGeodesic & {
+  intAngles: [number, number] | null // The angles of the interior points on the geodesic arc with respect to the center and radius. If isVertical is true, this will be null
+  intHeights: [number, number] | null // The heights of the interior points on the geodesic when the geodesic is vertical, null otherwise
+  length: number; // The hyperbolic distance between the two interior points
+};
+
+export type UhpGeodesicRay = UhpGeodesic & {
+  baseAngle: number | null // The angle the base point makes on the geodesic arc with respect to the center and radius. If isVertical is true, this will be null
+  headingRight: boolean | null // Whether the ray extends to the left/right from the base point. Is null when isVertical is true
+  baseHeight: number | null // The height of the base point on the geodesic arc when it is vertical, null otherwise
+  headingUp: boolean | null // Whether the ray extends up/down from the basepoint. Is null when isVertical is false
+}
 
 export type UhpCircle = {
   center: UhpInteriorPoint;
@@ -30,17 +55,37 @@ export type UhpCircle = {
 };
 
 export type UhpHorocycle = {
-  center: UhpInteriorPoint | PointAtInfinity;
   basePoint: UhpBoundaryPoint | PointAtInfinity;
-  bdryPoint: UhpInteriorPoint,
+  onHorPoint: UhpInteriorPoint;
+  center: UhpInteriorPoint | PointAtInfinity;
   eucRadius: number;
-}
-
-export type MobiusTransformation = {
-  a: ComplexNumber;
-  b: ComplexNumber;
-  c: ComplexNumber;
-  d: ComplexNumber;
 };
 
-export type Isometry = MobiusTransformation & { __brand: "Isometry" };
+export type UhpPolygon = {
+  vertices: UhpPoint[];
+  sides: UhpGeodesicSegment[];
+  angles: number[];
+  area: number;
+  perimeter: number;
+};
+
+export interface UhpIsometryInfo {
+  type: "hyperbolic" | "elliptic" | "parabolic" | "identity";
+  original: UhpIsometry;
+  standard: UhpIsometry;
+  conjugation: UhpIsometry;
+  fixedPoints:
+    | [UhpBoundaryPoint, UhpBoundaryPoint]  // hyperbolic
+    | UhpBoundaryPoint  // parabolic
+    | UhpInteriorPoint  // elliptic
+    | null  // identity, because it actually fixes all points
+  translationLength?: number // only for hyperbolic
+  axisOfTranslation?: UhpGeodesic // only for hyperbolic
+  angleOfRotation?: number // only for elliptic
+  parabolicDisplacement?: number // only for parabolic
+}
+
+export interface UhpConjugateInfo { 
+  isConjugate: boolean; 
+  conjugation: UhpIsometry | null
+}
