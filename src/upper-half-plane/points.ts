0a4ae642a148c62f6353a1ca84d42e6a56859db3
Nathan Lopez <nathancarllopez@gmail.com>
2025-09-09 15:32:04 -0500

    This class represents a point in the upper half plane model. The function getUhpPoints provides a set of constant values and a factory function. This allows the main class in index.ts to pass the tolerance once and update it in one place.

---
+++ src/upper-half-plane/points.ts
@@ -0,0 +1,52 @@
+import { ComplexNumber } from "../general-math/complex-numbers";
+import { isPositiveNumber } from "../util";
+
+export function getUhpPoints(tolerance: number = 1e-4): {
+  constants: Record<string, UhpPoint>;
+  factory: (re: number, im: number) => UhpPoint;
+} {
+  if (!isPositiveNumber(tolerance)) {
+    throw new Error("The tolerance must be a positive number");
+  }
+
+  return {
+    constants: {
+      ZERO: new UhpPoint(0, 0, tolerance),
+      ONE: new UhpPoint(1, 0, tolerance),
+      NEGONE: new UhpPoint(-1, 0, tolerance),
+      I: new UhpPoint(0, 1, tolerance),
+      NEGI: new UhpPoint(0, -1, tolerance),
+      INFINITY: new UhpPoint(Infinity, Infinity, tolerance),
+    },
+    factory: (re: number, im: number) => {
+      return new UhpPoint(re, im, tolerance);
+    },
+  };
+}
+
+export class UhpPoint extends ComplexNumber {
+  readonly type: "interior" | "boundary";
+  readonly subType?: "on-real-line" | "infinity";
+
+  constructor(re: number = 0, im: number = 0, tolerance: number = 1e-4) {
+    if (!isPositiveNumber(tolerance)) {
+      throw new Error("The tolerance must be a positive number");
+    }
+
+    if (im < 0) {
+      throw new Error("Imaginary part cannot be negative");
+    }
+
+    super(re, im, tolerance);
+
+    if (Number.isFinite(this.im) && this.im > 0) {
+      this.type = "interior";
+    } else {
+      this.type = "boundary";
+      this.subType =
+        re === Infinity && im === Infinity
+          ? "infinity"
+          : "on-real-line";
+    }
+  }
+}
