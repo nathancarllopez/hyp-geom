1386f8c8b3fd63e2dffbea215c00a3759de6eca4
Nathan Lopez <nathancarllopez@gmail.com>
2025-09-09 18:00:02 -0500

    Added the nearlyEqual function to introduce the idea of relative and absolute tolerances. I now need to go through the project and update things so that the previous tolerance (which was just an absolute tolerance) also includes the relative

--- src/util.ts
+++ src/util.ts
@@ -7,3 +7,10 @@ export const isPointArray = (arg: unknown): arg is [number, number] =>
   arg.length === 2 &&
   typeof arg[0] === "number" &&
   typeof arg[1] === "number";
+
+export const nearlyEqual = (
+  a: number,
+  b: number,
+  rtol: number = 1e-5,
+  atol: number = 1e-8
+) => Math.abs(a - b) <= atol + rtol * Math.max(Math.abs(a), Math.abs(b));
