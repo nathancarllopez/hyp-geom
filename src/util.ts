7bf6ceaea0b8a2ef06e2a52e6b785698372f9d51
Nathan Lopez <nathancarllopez@gmail.com>
2025-09-09 22:41:17 -0500

    1) Updated input type of isPositiveNumber to unknown to make it match isPointArray 2) Added checks for positivity of tolerances and infinity of inputs for nearlyEqual and anglesEquivalent

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,6 @@
 export type PositiveNumber = number & { __brand: "PositiveNumber" };
 
-export const isPositiveNumber = (num: number): num is PositiveNumber => num > 0;
+export const isPositiveNumber = (num: unknown): num is PositiveNumber => typeof num === "number" && num > 0;
 
 export const isPointArray = (arg: unknown): arg is [number, number] =>
   Array.isArray(arg) &&
@@ -14,6 +14,14 @@ export const nearlyEqual = (
   rtol: number = 1e-5,
   atol: number = 1e-8,
 ): boolean => {
+  if (!isPositiveNumber(rtol) || !isPositiveNumber(atol)) {
+    throw new Error("Tolerances must be positive");
+  }
+
+  if (Math.abs(a) === Infinity || Math.abs(b) === Infinity) {
+    return a === b;
+  }
+
   const absDiff = Math.abs(a - b);
   const relMultiplier = Math.max(Math.abs(a), Math.abs(b));
   const toleranceExpression = atol + rtol * relMultiplier;
@@ -26,7 +34,15 @@ export const anglesEquivalent = (
   b: number,
   atol: number = 1e-8,
 ): boolean => {
-  let diff = a - b;
+  if (Math.abs(a) === Infinity || Math.abs(b) === Infinity) {
+    throw new Error("Angles cannot be infinite");
+  }
+
+  if (!isPositiveNumber(atol)) {
+    throw new Error("Absolute tolerance must be positive");
+  }
+
+  let diff = Math.abs(a - b);
   diff = ((diff + Math.PI) % (2 * Math.PI)) - Math.PI;
 
   return Math.abs(diff) < atol;
